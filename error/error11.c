7be92874efe2a4054c691b17ef582a3e8f88d082
kongxiangyue <513001363@qq.com>
2020-10-25 09:37:43 +0800

    [A] 添加实例

---
+++ error/error11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main( )
+{
+	int i = 0;
+	int abc = 9; 
+	if (i == 0);
+		printf("iÎª0");
+	else
+		printf("i²»Îª0");
+		
+
+}
+
---
+++ error/error12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main( )
+{
+	int i = 0;
+	int abc = 9; 
+	if ((i+1 == 0)
+		printf("iÎª0");
+	else
+	
+		printf("i²»Îª0");
+	}
+		
+
+}
+
